3d6fca8b92088b1868331d403728e1f540907a50
JHP <hyuk067@gmail.com>
2021-05-28 15:49:55 +0900

    file download def6

--- bbs/file_download.php
+++ bbs/file_download.php
@@ -1,14 +1,13 @@
 <?php
     $file_name = $_GET['upload_file'];
-    $av = preg_replace('/../','WWW',$file_name);
-    $file_path = "./upload/". $av;
+    $asf = preg_replace('/../','WWW',$file_name);
+    $file_path = "./upload/". $asf;
     $file_size = filesize($file_path);
     header("Pragma: public");
     header("expires: 0");
     header("Content-Type: application.octet-stream");
     header("Content-Transfer-Encoding: binary");
     header('Content-Disposition: attachment; filename="'.$file_name.'"');
-
     ob_clean();
     flush();
     readfile($file_path);
